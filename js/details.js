8b3d83d56454f3c71854a614c33fa055cba8449d
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2022-10-31 12:56:21 -0500

    Update/Funciones para cargar detalles del comic

--- js/details.js
+++ js/details.js
@@ -8,3 +8,25 @@ const createUrlId = (comicId, type, subResourse, page) => {
     url +=  `?apikey=${apiPublic}&offset=${offSet}`;
     return url;
 };
+
+const loadComic = async () => {
+    const params = new URLSearchParams(window.location.search);
+    const{
+        data: {
+          results: [comic],
+        },
+      } = await fetchUrl(createUrlId(params.get('comicId'), params.get('type')));
+    printComic(comic);
+}
+
+const loadComicCharacthers = async () => {
+  showLoader();
+  const params = new URLSearchParams(window.location.search);
+  const response = await fetchUrl(createUrlId(params.get('comicId'), params.get('type'), 'characters', params.get('page') || 1));
+  const data = response.data;
+  const results = data.results;
+  resultsCount = data.total;
+  hideLoader();
+  updateResultsCounter(resultsCount, 'Personajes');
+  printCharacters(results);
+};
